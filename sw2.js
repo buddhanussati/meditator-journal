7025297bc09040f0235c4c5961833ac00c465e16
buddhanussati <buddhanussati@gmail.com>
2026-01-14 13:14:51 +0700

    Update sw2.js

--- sw2.js
+++ sw2.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'meditator-journal-app-v4';
+const CACHE_NAME = 'meditator-journal-app-v5';
 // Danh sách các file cần lưu để chạy offline
 const ASSETS = [
   './',
@@ -44,4 +44,5 @@ self.addEventListener('fetch', (event) => {
       return response || fetch(event.request);
     })
   );
+
 });
